16bc90907b0448ac854a6506129f03578df3f2c8
Bednyakov <bednyakov99@gmail.com>
2022-11-08 11:24:47 +0300

    Исправил анимацию нажатия кнопки
    Теперь при быстром отжатии кнопка корректно поднимается

--- Assets/_Project/Scripts/MainButton/MainButtonAnimator.cs
+++ Assets/_Project/Scripts/MainButton/MainButtonAnimator.cs
@@ -7,17 +7,21 @@ namespace _Project.Scripts.MainButton
 	{
 		private const float PUSH_END_Z_VALUE = -0.00758f;
 		private const float PUSH_DURATION = 0.3f;
+		private const float UNPUSH_END_Z_VALUE = -0.005275526f;
+		private const float UNPUSH_DURATION = 0.1f;
 
 		[SerializeField] private Transform _pushTransform = null!;
 
 		public void PlayPush()
 		{
+			DOTween.Kill(_pushTransform);
 			_pushTransform.DOLocalMoveZ(PUSH_END_Z_VALUE, PUSH_DURATION);
 		}
 
 		public void PlayUnpush()
 		{
-			_pushTransform.DOLocalMoveZ(-0.005275526f, 0.1f);
+			DOTween.Kill(_pushTransform);
+			_pushTransform.DOLocalMoveZ(UNPUSH_END_Z_VALUE, UNPUSH_DURATION);
 		}
 	}
 }
