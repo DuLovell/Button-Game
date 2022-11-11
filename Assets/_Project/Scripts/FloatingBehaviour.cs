1ef908b9273ddcf404ebc5a82405ec784a911597
DuLovell <bednyakov99@gmail.com>
2022-11-11 18:52:24 +0300

    Добавил скрипт, который заставляет объект как бы парить в воздухе
    Нужен для npc

---
+++ Assets/_Project/Scripts/FloatingBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace _Project.Scripts
+{
+	public class FloatingBehaviour : MonoBehaviour
+	{
+		private const float MOVE_RANGE = 0.05f;
+		private const float MOVE_DURATION = 1;
+
+		private Vector3 _velocity = Vector3.zero;
+		private float _directionModifier = 1;
+
+		private void Awake()
+		{
+			StartCoroutine(FloatingRoutine());
+		}
+
+		private IEnumerator FloatingRoutine()
+		{
+			Vector3 centralPoint = transform.position;
+
+			while (true) {
+				_velocity = Vector3.zero;
+				Vector3 targetPosition = new(centralPoint.x, centralPoint.y + (_directionModifier * MOVE_RANGE), centralPoint.z);
+				while (Vector3.Distance(transform.position, targetPosition) > 0.01f)
+				{
+					transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref _velocity, MOVE_DURATION);
+					yield return null;
+				}
+				_directionModifier *= -1;
+			}
+			// ReSharper disable once IteratorNeverReturns
+		}
+	}
+}
