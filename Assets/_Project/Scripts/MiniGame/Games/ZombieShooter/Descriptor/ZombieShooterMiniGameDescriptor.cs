e63217de5e9e962ab2658d14a94274393278c525
Bednyakov <bednyakov99@gmail.com>
2022-11-22 09:47:43 +0300

    Добавил дескриптор для ZombieShooter

---
+++ Assets/_Project/Scripts/MiniGame/Games/ZombieShooter/Descriptor/ZombieShooterMiniGameDescriptor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace _Project.Scripts.MiniGame.Games.ZombieShooter.Descriptor
+{
+	[CreateAssetMenu(fileName = "ZombieShooterMiniGameDescriptor", menuName = "Data/MiniGames/ZombieShooter/Descriptor", order = 0)]
+	public class ZombieShooterMiniGameDescriptor : ScriptableObject
+	{
+		[field: Header("Ammo")]
+		[field: SerializeField]
+		public int StartAmmoCount { get; private set; }
+		[field: SerializeField]
+		public int HeadshotBonusAmmo { get; private set; }
+		
+		[field: Header("Aim")]
+		[field: SerializeField]
+		public float StartAimMoveSpeed { get; private set; }
+		[field: SerializeField]
+		public float NextRoomAimMoveSpeedDelta { get; private set; }
+		
+		[field: Header("Zombies")]
+		[field: SerializeField]
+		public int InRoomZombieCount { get; private set; }
+		[field: SerializeField]
+		public GameObject ZombiePrefab { get; private set; } = null!;
+	}
+}
